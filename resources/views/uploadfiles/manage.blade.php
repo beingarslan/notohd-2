a3b7e8fc059967548aa71d44b538558016e90f0e
ARSLAN ASLAM <39445256+beingarslan@users.noreply.github.com>
2022-01-30 01:54:59 +0500

    Update manage.blade.php

--- resources/views/uploadfiles/manage.blade.php
+++ resources/views/uploadfiles/manage.blade.php
@@ -20,20 +20,11 @@
         <div class="col-md-3 col-xl-3">
             <div class="card mb-3">
                 <img class="card-img-top lazy" src="{{$image->thumbnail}}" alt="Card image cap" />
-                <div class="card-body">
-                    <!-- <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in to additional content. This content is
-            a little bit longer.
-          </p>
-          <p class="card-text">
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </p> -->
-                </div>
             </div>
         </div>
         @endforeach
         <div class="mb-3">
-        {{ $images->links() }}
+            {{ $images->links() }}
         </div>
 
 
